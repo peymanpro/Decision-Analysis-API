9cd2cee29c00fe1d3efd60dab3fd4882802c6c9a
Peyman Salimi <pmnslm6344@gmail.com>
2026-09-11 17:31:33 +0330

    chore: remove unused account model and secure api defaults

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
